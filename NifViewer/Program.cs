1b3d068ad4048a2fc0fdd08e5377a8aa4a6b9dfb
Roman Shapiro <eleroman@mail.ru>
2026-09-21 14:27:08 +0700

    Show usage if path isn't provided

--- NifViewer/Program.cs
+++ NifViewer/Program.cs
@@ -6,6 +6,13 @@ class Program
 {
 	static int Main(string[] args)
 	{
+		if (args.Length == 0)
+		{
+			Console.WriteLine("Usage: NifViewer <skyrim-folder>");
+			Console.WriteLine("  <skyrim-folder>  Path to the Skyrim installation folder, e.g. \"D:\\SteamLibrary\\steamapps\\common\\Skyrim Special Edition\"");
+			return 1;
+		}
+
 		try
 		{
 			Environment.SetEnvironmentVariable("FNA3D_FORCE_DRIVER", "D3D11");
